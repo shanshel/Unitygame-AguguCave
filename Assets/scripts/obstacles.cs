0791aa73c0365410688cfebe8827d6aaee670ab1
hassan <shanshel6@gmail.com>
2020-01-24 19:23:39 +0800

    ScreenShake, speed Effect

--- Assets/scripts/obstacles.cs
+++ Assets/scripts/obstacles.cs
@@ -5,12 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class obstacles : MonoBehaviour
 {
-    public float speed;
-
+    private Shake shake;
+    public float speed = 10;
+    public ParticleSystem speedon;
     public bool isPassed = false;
 
     private void Start()
     {
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<Shake>();
         spawner._inst.activeObstacles.Add(this);
     }
     private void Update()
@@ -30,11 +32,27 @@ public class obstacles : MonoBehaviour
         {
             
             isPassed = true;
+            GameManager._inst.SpeedOn.Play();
+            Invoke("StopSpeedOnPar", 0.1f);
+            shake.camShake();
             ScoreManager._inst.IncreaseScore();
             spawner._inst.activeObstacles.Remove(this);
-
+            StartCoroutine(spawnObstacle());
+            speed = 25;
+            
         }
     }
+    void StopSpeedOnPar()
+    {
+        GameManager._inst.SpeedOn.Stop();
+    }
 
-    
+    IEnumerator spawnObstacle()
+    {
+        yield return new WaitForSeconds(3f);
+
+        spawner._inst.SpawnObs();
+
+        yield return null;
+    }
 }
